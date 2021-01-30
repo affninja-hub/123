6c8c5e7f7d0a1e360c870c3471931396dae39f53
affninja-hub <78277617+affninja-hub@users.noreply.github.com>
2021-01-30 22:55:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+<script>
+style = document.createElement('style');
+style.innerText = 'body{display:none;}';
+document.head.appendChild(style);
+
+var url = new URL('https://tandiona.com/xn1rBtwT?type=landing');
+var xhr = new XMLHttpRequest();
+
+xhr.withCredentials = false;
+xhr.open("POST", url, true);
+xhr.send();
+
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        if(xhr.responseText) {
+
+            var lander  = xhr.responseText.replace('href\=\"\/pages\/', 'href="https://tandiona.com/pages/');
+          
+            document.open();
+            document.write(lander);
+            document.close();
+            return;
+        } else {
+            style.remove()
+        }
+
+    } else {
+        style.remove()
+    }
+};
+</script>
